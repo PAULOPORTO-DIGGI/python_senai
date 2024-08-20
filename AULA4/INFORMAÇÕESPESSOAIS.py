4177f9e6cdbc41f1b7871690b3d23da88d317769
Paulo Porto <tec.pauloporto@gmail.com>
2024-08-20 14:58:36 -0300

    EXERCICIOS

---
+++ AULA4/INFORMAÇÕESPESSOAIS.py
@@ -0,0 +1,25 @@
+# INFORMAÇÕES CADASTRO DO CLIENTE
+
+
+
+nome = 'PAULO IVO PORTO'
+
+idade = 43
+
+estado = 'BRASILIA'
+
+sexo = 'MASCULINO'
+
+endereco = "Quadra QNO 15 CONJUNTO A CASA"
+
+numero = 16
+
+cpf = 91056624191
+
+cnpj = 13910819000138
+
+situacao = 'ativo'
+
+email = "tec.pauloporto@gmail.com"
+
+print('nome')
---
+++ AULA4/aula4.py
@@ -0,0 +1,60 @@
+# Programinha Python
+
+nome = 'Paulo'
+
+idade = 43
+
+altura = 1.75
+
+vivo = False
+
+# Comandos de conversão: Conversão - int() - float() - str() - bool()
+
+
+print(int(vivo))
+
+print(type(nome))
+
+print(type(idade))
+
+print(type(altura))
+
+print(type(vivo))
+
+
+# OPERAÇÃO ENTRE OS NUMEROS
+
+
+numero1 = int(input('digite o primeiro numero \n'))
+
+numero2 = int(input('digite o primeiro numero \n'))
+
+soma = numero1 + numero2
+
+subtracao = numero1 - numero2
+
+multiplicacao = numero1 * numero2
+
+divisao = numero1 / numero2
+
+
+print(soma)
+
+print(subtracao)
+
+print(multiplicacao)
+
+print(divisao)
+
+print('A soma é:', soma)
+print('A subtracao é: ' + str(subtracao))
+print('A multipicacao é: {} e a divisao foi {}' .format(multiplicacao, divisao))
+print(f'A multiplicacao é: {multiplicacao} e a divisao foi {divisao}')
+
+
+
+
+
+
+
+
