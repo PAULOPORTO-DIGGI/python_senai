bc8bc55c687dc31da281223fcafb92ab742d033f
Paulo Porto <tec.pauloporto@gmail.com>
2024-08-20 18:00:12 -0300

    AULA 5 CONDIÇÃO

--- AULA05/AULA05.py
+++ AULA05/AULA05.py
@@ -23,18 +23,79 @@
 # print(numero1 != numero2)
 
 
-#### CONDIÇÃO
-# if
-# else
+#### CONDIÇÃO/COMPARAÇÃO
+# if (SE)
+# else (SENÃO)
+## elif (Se não se)
 
-idade = int(input('Informe sua idade: \n'))
 
-if (idade >= 18):
 
-    print('maior de idade')
+# idade = int(input('Informe sua idade: \n'))
 
+# if (idade >= 120):
+#     print('idade invalidade')
+# elif(idade > 18):
+#     print('maior de idade')
+# elif(idade<0):
+#     print('ainda não nasceu')
+
+# else:
+#     print('menor de idade')
+
+
+
+
+
+# idade = int(input('Informe sua idade: \n'))
+
+
+# if(idade >= 18):
+#     print('Pode assisitr o filme')
+# elif(idade >= 16):
+#     acompanhado = input('Esta acompanhado de adulto sim ou não? \n')
+#     if (acompanhado == 'sim'):
+#         print('pode assistir')
+#     else:
+#         print('não pode assistir')
+# else: 
+#     print('não pode assistir')
+
+
+
+
+
+##idade = int(input('Informe sua idade: \n'))
+
+##if (idade < 18):
+    ##acompanhado = input('Esta acompanhado de adulto sim ou não? \n')
+    ##if (acompanhado == 'sim'):
+       ## print('pode assistir')
+    ##else:
+        ##print('não pode assistir')
+##else:
+    ##print('não pode assisitr')
+    
+
+## "e" - and
+## "OU" - or
+
+# Sempre que tiver um bloco de comandos utilizar dois pontos ":"
+
+alladin = input('alladin apareceu? \n')
+jasmine = input('jasmine apareceu? \n')
+
+if (alladin == 'sim') and (jasmine == 'sim'):
+    print('love a noite inteira')
 else:
-    print('menor de idade')
+    print('não rolou encontro')
+
+    
+
+
+
+
+
+
 
 
 
