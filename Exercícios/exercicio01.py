b0a1a55a6cc4102579f500bd49beb781cd3abdc8
Paulo Porto <tec.pauloporto@gmail.com>
2024-08-21 17:47:22 -0300

    trabalho

---
+++ Exercícios/exercicio01.py
@@ -0,0 +1,8 @@
+numero1 = 1
+numero2 = 2
+numero3 = 3
+
+numero = int(input('Digite um numero de 1 a 3 \n'))
+if numero ==1:
+    print('')
+
---
+++ aula06/aula06.py
@@ -0,0 +1,179 @@
+
+### match case uma estrutura condicional, alternativa ao if
+
+### abaixo verifica o voto do usuário
+
+
+# candidato = int(input('informe o numero do candidato'))
+
+# if candidato == 13:
+#     print('Votou no Lula')
+# elif candidato == 22:
+#     print('Votou no Bolsonara')
+
+# else:
+#     print('Candidato Inválido')
+
+
+# candidato = int(input('informe o numero do candidato'))
+
+# match candidato:
+#     case 13:
+#         print("Votou no Lula")
+#     case 22:
+#         print("Votou no Bolsonaro")
+#     case _:
+#         print('opção invalida')
+
+
+
+########### ATRIBUIR VALORES A UMA VARIAVEL (CONTADORES)
+
+# numero = 10
+# print(numero)
+
+# numero = numero + 10
+# print(numero)
+
+# numero = numero - 10
+# print(numero)
+
+# numero = numero *10
+# print(numero)
+
+# numero = numero / 10
+# print(numero)
+
+
+#### Reatribuição de váriavel, "está reaproveitando o valor e adicionando outro" SÃO ESTRUTURAS CONDICIONAIS IF E ELSE
+
+# numero = 10
+# print(numero)
+
+# numero = numero + 10
+# numero += 10
+# print(numero)
+
+# numero = numero - 10
+# numero -= 10
+# print(numero)
+
+
+# numero = numero *10
+# numero *10
+# print(numero)
+
+# numero = numero / 10
+# numero /= 10
+# print(numero)
+
+
+### VERIFICADDO SE UM NUMERO É PAR OU IMPAR
+
+# numero = int(input('informe o numero'))
+
+# if numero % 2 == 0:
+#     print('numero é par')
+# else:
+#     print('numero é impar')
+
+### LAÇOS DE REPETIÇÃO LOOP (RANGE SERIA O INTERVALO DE VEZES )
+
+# for i in range(5):
+#     print('PAULO')
+
+# for i in range(5):
+#     print('i')
+
+# nomes = ['Luciano', 'Lucas', 'Arthur', 'Aline',]
+    
+#         print(nomes)
+
+#         for nome in nomes:
+#         print(nome)
+
+
+#         frutas = ['banana', 'maçã', 'morango', 'laranja']
+#         for frutas in frutas:
+#             print(fruta)
+
+# print(frutas[2])
+
+
+# frutas = ['banana', 'maça', 'morango', 'laranja']
+# for frutas in frutas:
+#         print(frutas)
+
+# for indice, fruta in enumerate(frutas):
+#     print(f'Suas frutas são {indice}: {fruta}')
+
+# nomes = []
+# for i in range(5):
+#     nome = input('Informe o seu nome: \n')
+#     nomes.append(nome)
+
+# for nome in nomes:
+#     print(nome)
+
+# nome = 'Paulo'
+
+# for i in nome:
+#     print(i)
+
+
+### While em português 'ENQUANTO' Repete até a condição ser atendida.
+
+# numero = None
+
+# while numero != 0:
+#     numero = int(input('informe o numero \n'))
+
+
+# contador = 12
+# numero = int(input('informe o numero'))
+
+# while contador < 10:
+#     print(numero * 2)
+
+# contador = 10
+# numero = int(input('informe o numero'))
+
+# while contador < 10:
+#     print(numero * 2)
+
+# numero = 10
+# while True:
+#     numero *= 10
+#     print(numero)
+
+
+# contador = 10
+# numero = int(input('informe o numero'))
+
+# while contador < 10:
+#     print(numero * 2)
+
+# numero = 10
+# while True:
+#     numero *= 10
+#     print(numero)
+# if numero > 100000000:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+    
